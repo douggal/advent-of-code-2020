f65343f526985ce14338779085cee53cb316b49d
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2020-12-05 12:38:30 -0500

    Day 4 rename Passport class file

--- Day04_PassportProcessing/Passport.cs
+++ Day04_PassportProcessing/Passport.cs
@@ -2,28 +2,29 @@ using System.Collections.Generic;
 
 namespace Day04_PassportProcessing
 {
-    public class Passportz
+    public class Passport
     {
 
-        public Passportz()
+        public Passport()
         {
             Complete = false;
-            PassportFields = new List<string> { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid" };
+            RequiredPassportFields = new List<string> { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid" };
             _passportItems = new Dictionary<string, string>();
         }
 
         private Dictionary<string, string> _passportItems;
         public Dictionary<string, string> PassportItems
         {
+            // it's possible passport could have items other than 8 required fields
             get { return _passportItems; }
         }
 
-        public List<string> PassportFields { get; set; }
+        public List<string> RequiredPassportFields { get; set; }
         public bool Complete { get; set; }
 
         public bool IsComplete()
         {
-            foreach (var f in PassportFields)
+            foreach (var f in RequiredPassportFields)
             {
                 if (f == "")
                 {
