6a764d88fba2efa4920499418721f7c05e1038cd
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2020-12-06 14:26:46 -0500

    Add a Day 6 part 2 sln and refactor python like

--- Day06_CustomCustoms/custom_customs.py
+++ Day06_CustomCustoms/custom_customs.py
@@ -1,4 +1,5 @@
 #   Advent of Code 2020  Day 6 Puzzle Custom Customs https://adventofcode.com
+#   Created 12/06/2020
 
 import re
 import collections
@@ -9,35 +10,55 @@ print('--- Day 6: Custom Customs ---')
 print()
 
 fn = "PuzzleInput.txt"
-grpDict = {}
-groupCtr = 0
-grpDict[groupCtr] = {}
+grp_dict = {}
+grp_ctr = 0
+grp_dict[grp_ctr] = {}
+grp_count = {}
+grp_count[0] = 0
+count_of_letters = 26
 
 with open(fn) as f:
     for line in f:
         if line.strip() == "":
             # New group
-            groupCtr += 1
-            grpDict[groupCtr] = {}
+            grp_ctr += 1
+            grp_dict[grp_ctr] = {}
+            grp_count[grp_ctr] = 0
         else:
+            grp_count[grp_ctr] += 1
             for c in line.strip():
                 if re.match('[a-z]', c):  #ignore junk data if there is any
-                    grpDict[groupCtr][c] = {}
-                    grpDict[groupCtr][c] = 1
+                    if c not in grp_dict[grp_ctr]:
+                        grp_dict[grp_ctr][c] = {}
+                        grp_dict[grp_ctr][c] = 1
+                    else:
+                        grp_dict[grp_ctr][c] += 1
 
 # Part 1 of the Day 6 puzzle:
 gsum = 0
 total = 0
-for grp in grpDict:
+for grp in grp_dict:
     gsum = 0
-    for customs_form in grpDict[grp]:
+    for customs_form in grp_dict[grp]:
         gsum += 1
     total += gsum
     #print(grp, grpDict[grp], ' group sum = ', gsum, ' total so far = ', total)
     #print()
 
 print()
-print('Part 1:  What is the sum of those counts?  ', total)
+print('Part 1:  What is the sum of the customs forms ''yes'' counts?  ', total)
 
 # Part 2 of the Day 6 puzzle:
+gsum = 0
+total = 0
+count_of_grp = 0
+for grp in grp_dict:
+    gsum = 0
+    for ques in grp_dict[grp]:
+        if grp_dict[grp][ques] == grp_count[grp]:
+            gsum += 1
+    total += gsum
+
+print()
+print('Part 2:  What is the sum of the customs forms counting only q''s with all in group = ''yes'' counts?  ', total)
 
