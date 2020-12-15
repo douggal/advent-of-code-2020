4985a951e7198f989be6057b7a49d192565efc35
douggal <8922663+douggal@users.noreply.github.com>
2020-12-15 00:56:51 -0500

    Day 14 part 1 complete (#9)
    * Day14 init()
    
    * Day 14 pt 1 plan B
    
    * Day 14 pt 1 working draft
    
    * Day 14 part 1 complete

---
+++ Day14_DockingData/docking_data.py
@@ -0,0 +1,58 @@
+#   Advent of Code 2020  Day 14 Puzzle Docking Data https://adventofcode.com
+#   Created 12/14/2020
+
+import re
+import collections
+from array import array
+
+print('Advent of Code 2020')
+print('--- Day 14: Docking Data ---')
+print()
+
+# Python find letter in list
+# https://stackoverflow.com/questions/26355191/python-check-if-a-letter-is-in-a-list
+# slicing string
+# https://stackoverflow.com/questions/663171/how-do-i-get-a-substring-of-a-string-in-python
+
+
+#count = 65535
+#sea_port_comp = array('Q', (0x0 for i in range(count)))
+sea_port_comp = {}
+
+fn = "PuzzleInput.txt"
+with open(fn) as f:
+    for line in f:
+        if (line.strip() != ""):
+            l = line.strip()
+            if l[:4] == "mask":
+                mask = l[7:]
+            else:
+                instr = re.split(r' = ',l)
+                v = int(instr[1])
+                j = 0
+                for i in reversed(range(0,36)):
+                    if j==35:
+                        s = mask[35:]
+                    else:
+                        s = mask[j:j+1]
+                    if s =='1':
+                        # number |= 1UL << n;   SET BIT I to a 1
+                        v |= 0x1 << i
+                    elif s=='0':
+                        # https://stackoverflow.com/questions/47981/how-do-you-set-clear-and-toggle-a-single-bit
+                        # number &= ~(1UL << n);  CLEAR BIT I (  = 0)
+                        v &= ~(0x1 << i)
+                    j += 1
+                sea_port_comp[instr[0]] = v
+
+#print(mask)
+print(sea_port_comp)
+
+# https://stackoverflow.com/questions/4880960/how-to-sum-all-the-values-in-a-dictionary
+print('Day 14 Part 1 sum of the values in the sea port computer is: ',sum(sea_port_comp.values()))
+
+
+
+# Part 2 of the Day 14 puzzle:
+print()
+print('Part 2:  ? ')
