caf64dd6cbdd9c8a6e8457a0b21b163cec0dbd66
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2020-12-15 00:46:12 -0500

    Day 14 pt 1 working draft

--- Day14_DockingData/docking_data.py
+++ Day14_DockingData/docking_data.py
@@ -6,7 +6,7 @@ import collections
 from array import array
 
 print('Advent of Code 2020')
-print('--- Day 14: Shuttle Search ---')
+print('--- Day 14: Docking Data ---')
 print()
 
 # Python find letter in list
@@ -26,26 +26,24 @@ with open(fn) as f:
             l = line.strip()
             if l[:4] == "mask":
                 mask = l[7:]
-                # s = s[::-1]
-                # print(s)
-                # for i in range(0,35):
-                #     if s[i] == "1":
-                #         mask += 0x1 * 2**(i*2)
             else:
                 instr = re.split(r' = ',l)
                 v = int(instr[1])
-                m1 = 0x1  # mask 1s
-                m0 = 0x1  # mask 0s
-                for i in range(35,0):
-                    if mask[i+1:i]=='1':
-                        # number &= ~(1UL << n);
-                        m1 &= ~(0x1 << (2**i))
-                    elif (mask[i+1:i]=='0'):
+                j = 0
+                for i in reversed(range(0,36)):
+                    if j==35:
+                        s = mask[35:]
+                    else:
+                        s = mask[j:j+1]
+                    if s =='1':
+                        # number |= 1UL << n;   SET BIT I to a 1
+                        v |= 0x1 << i
+                    elif s=='0':
                         # https://stackoverflow.com/questions/47981/how-do-you-set-clear-and-toggle-a-single-bit
-                        # number |= 1UL << n;
-                        m0 |= 0x1 << (2**i)
-                sea_port_comp[instr[0]] = v & m1 # bitwise AND
-                sea_port_comp[instr[0]] = v | m0 # bitwise OR set bit to a 0
+                        # number &= ~(1UL << n);  CLEAR BIT I (  = 0)
+                        v &= ~(0x1 << i)
+                    j += 1
+                sea_port_comp[instr[0]] = v
 
 print(mask)
 print(sea_port_comp)
