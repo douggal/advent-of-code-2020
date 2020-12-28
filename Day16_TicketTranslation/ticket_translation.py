8105135b69c698c730c6b75144342d719e8795e3
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2020-12-27 22:30:41 -0500

    Day 16 part 1 start

--- Day16_TicketTranslation/ticket_translation.py
+++ Day16_TicketTranslation/ticket_translation.py
@@ -10,11 +10,32 @@ print('--- Day 16: Ticket Translation ---')
 print()
 
 
-fn = "PuzzleInput.txt"
+fn = "PuzzleInputTest.txt"
+read_rules = True
+rules = dict()
+my_tkt = []
+nearby_tkts = []
 with open(fn) as f:
     for line in f:
-        if (line.strip() != ""):
-            l = line.strip()
+        # read first group: the rules
+        if (read_rules and line.strip() != ""):
+            l = line.strip().split(':')
+            name = re.sub(r' ','_',l[0])
+            rngs = re.sub(r' or ','-',l[1])
+            hilo= rngs.split('-')
+            rules[name] = [int(hilo[0].strip()),int(hilo[1].strip()),int(hilo[2].strip()),int(hilo[3].strip())]
+        else:
+            read_rules = False
+            continue
+
+        # next my ticket
+        
+        
+
+        # next other tickets
+
+print(rules)
+
 
 
 print('Day 16 Part 1:  my ticket scanning error rate is ')
